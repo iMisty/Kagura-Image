380f69f489128bf8b15ea29d9dd7f0692dd15cba
Miya <qianwumeng_2cyndi@qq.com>
2019-11-06 00:02:25 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,24 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import vuei18n from 'vue-i18n';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(vuei18n);
+
+const i18n = new vuei18n({
+  locale: 'zh',
+  messages: {
+    'zh': require('./assets/lang/zh'),
+    'en': require('./assets/lang/en'),
+    'jp': require('./assets/lang/jp')
+  }
+});
 
 new Vue({
   router,
   store,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
