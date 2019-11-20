d448ecea4859ef4cdb733e92060f44858b755ea3
Miya <qianwumeng_2cyndi@qq.com>
2019-11-21 00:09:21 +0800

    Create Contact.vue

---
+++ src/views/Contact.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class=""></div>
+</template>
+
+<script>
+
+export default {
+    components: {},
+    data() {
+        return {
+
+        };
+    },
+    computed: {},
+    watch: {},
+    methods: {},
+    created() {},
+    mounted() {},
+    beforeCreate() {},
+    beforeMount() {},
+    beforeUpdate() {},
+    updated() {},
+    beforeDestroy() {},
+    destroyed() {},
+    activated() {},
+    }
+</script>
+<style lang='less' scoped>
+//@import url(); 引入公共css类
+
+</style>
