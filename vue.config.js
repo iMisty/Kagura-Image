0b8d98207fc3aca2b48bf595255a3c1cef598ecd
Miya <qianwumeng_2cyndi@qq.com>
2020-02-21 05:57:32 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,25 @@
+const path = require('path');
+
 module.exports = {
-  devServer: {
-      proxy: {
-          '/api': {
-              target: 'https://blog.dressweb.cn',
-              // 允许跨域
-              changeOrigin: true,
-              ws: true,
-              pathRewrite: {
-                  '^/api': ''
-              }
-          }
-      }
-  }
+	devServer: {
+		proxy: {
+			'/api': {
+				target: 'https://blog.dressweb.cn',
+				// 允许跨域
+				changeOrigin: true,
+				ws: true,
+				pathRewrite: {
+					'^/api': ''
+				}
+			}
+		}
+	},
+	configureWebpack: {
+		resolve: {
+			alias: {
+				'assets': path.resolve('./src/assets'),
+				'@': path.resolve('./src')
+			}
+		}
+	},
 }
