2a42809482290687b6173f81dc87f31b2a955a24
Miya <qianwumeng_2cyndi@qq.com>
2020-02-21 05:58:29 +0800

    finished index banner

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,64 +1,65 @@
 <template>
-    <div class="home">
-      <ul>
-        <li v-for="list in ace" :key="list.index">
-          <div v-html="list.content">
-            {{list.content}}
-          </div>
-          </li>
-      </ul>
-    </div>
+  <div class="home">
+    <banner :link="background.link" :name="background.name">
+      <div>Test text 1</div>
+      <div>Test text 2</div>
+    </banner>
+  </div>
 </template>
 
 <script>
+import banner from "@/components/Home/banner.vue";
 export default {
-    components: {},
-    data() {
-        return {
-          info:{},
-          ace:{},
-          id:{}
-    }
-    },
-    computed: {},
-    watch: {},
-    methods: {
-      getInfo(){
-        this.$axios.post('/api/?json=1')
-        .then(res => (
+  components: {
+    banner
+  },
+  data() {
+    return {
+      background: {
+        link: require("@/static/img/bg.jpg"),
+        name: "Logo"
+      },
+      bannerText: ''
+    };
+  },
+  computed: {},
+  watch: {},
+  methods: {
+    getInfo() {
+      this.$axios
+        .post("/api/?json=1")
+        .then(
+          res =>
             // eslint-disable-next-line no-console
-          this.info = res
-        ))
-        .then(res => (
-          this.ace = res.data.posts
-        ))
+            (this.info = res)
+        )
+        .then(res => (this.ace = res.data.posts))
         // eslint-disable-next-line no-console
-        .catch(function (error) {
+        .catch(function(error) {
           // eslint-disable-next-line no-console
           console.log(error);
-        })
-      },
-      getID(){
-        const a = this.ace;
-        // eslint-disable-next-line no-console
-        console.log(a);
-      }
+        });
     },
-    created() {},
-    mounted() {
-      this.getInfo();
-      this.getID();
-    },
-    beforeCreate() {},
-    beforeMount() {},
-    beforeUpdate() {},
-    updated() {},
-    beforeDestroy() {},
-    destroyed() {},
-    activated() {},
+    getID() {
+      const a = this.ace;
+      // eslint-disable-next-line no-console
+      console.log(a);
     }
+  },
+  created() {},
+  mounted() {
+    //this.getInfo();
+    //this.getID();
+  },
+  beforeCreate() {},
+  beforeMount() {},
+  beforeUpdate() {},
+  updated() {},
+  beforeDestroy() {},
+  destroyed() {},
+  activated() {}
+};
 </script>
 <style lang='less' scoped>
 //@import url(); 引入公共css类
-
 </style>
