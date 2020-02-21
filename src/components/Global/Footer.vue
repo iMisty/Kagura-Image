f7d65a5c3c61238ff443f7df6f504171fd940987
Miya <qianwumeng_2cyndi@qq.com>
2020-02-22 04:58:08 +0800

    Import global module

---
+++ src/components/Global/Footer.vue
@@ -0,0 +1,39 @@
+<template>
+<div class="footer">
+
+</div>
+</template>
+
+<script>
+export default {
+//组件
+ components: {
+ },
+//静态
+ props: {
+ },
+//保存数据用
+ data() {
+   return{
+   }
+ },
+//对象内部的属性监听，也叫深度监听
+ watch: {
+ },
+//属性的结果会被缓存，除非依赖的响应式属性变化才会重新计算。主要当作属性来使用；
+ computed: { 
+ },
+//方法表示一个具体的操作，主要书写业务逻辑；
+ methods: { 
+ },
+//请求数据
+ created() {
+},
+ mounted() {
+}
+}
+</script>
+
+<style lang='less' scoped>
+
+</style>
