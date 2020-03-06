0f3c7d6de4a969bd18db1cb41a8fafcd40fe9387
Miya <qianwumeng_2cyndi@qq.com>
2020-03-07 01:47:12 +0800

    Update Blogs.vue

--- src/views/Blogs.vue
+++ src/views/Blogs.vue
@@ -1,33 +1,52 @@
 <template>
-    <div class="blogs">
-        This is blog
+  <div class="blogs">
+    <div class="wrap">
+      <inner-title :imgLink="title.background"></inner-title>
+      <div class="record-list">
+        <list></list>
+        <list></list>
+        <list></list>
+        <list></list>
+        <list></list>
+        <list></list>
+        <list></list>
+        <list></list>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-
+import innerTitle from "@/components/Global/inner-title.vue";
+import list from "@/components/Blogs/list.vue";
 export default {
-    components: {},
-    data() {
-        return {
-
-        };
-    },
-    computed: {},
-    watch: {},
-    methods: {},
-    created() {},
-    mounted() {},
-    beforeCreate() {},
-    beforeMount() {},
-    beforeUpdate() {},
-    updated() {},
-    beforeDestroy() {},
-    destroyed() {},
-    activated() {},
-    }
+  components: {
+    innerTitle,
+    list
+  },
+  data() {
+    return {
+      title: {
+        background: require("@/static/img/test1.jpg")
+      }
+    };
+  },
+  computed: {},
+  methods: {},
+  created() {},
+  mounted() {}
+};
 </script>
 <style lang='less' scoped>
-//@import url(); 引入公共css类
-
+.record-list {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 6rem;
+	text-align: left;
+	padding-top: 40px;
+	padding-bottom: 80px;
+  p {
+    margin: 0;
+  }
+}
 </style>
