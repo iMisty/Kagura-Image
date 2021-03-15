27b5b4c2cc5dc13b91ec1b43d4bf63923045d92c
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2021-03-15 18:12:12 +0800

    Upload Components

---
+++ front/src/components/update.tsx
@@ -0,0 +1,19 @@
+/*
+ * @Author: Miya
+ * @Date: 2021-03-15 18:05:02
+ * @LastEditTime: 2021-03-15 18:07:16
+ * @LastEditors: Miya
+ * @Description: 拖拽上传文件组件
+ * @FilePath: \maid-chanc:\Users\Platinum Prism\Documents\GitHub\Kagura-Image\front\src\components\update.tsx
+ * @Version: 1.0
+ */
+import { defineComponent } from 'vue';
+
+const Update = defineComponent({
+  setup() {},
+  render() {
+    return <div class="update">Update</div>;
+  },
+});
+
+export default Update;
