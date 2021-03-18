b4348e17da2433b26bbb1ceacbfc2108cefcd66e
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2021-03-18 18:15:23 +0800

    Update: Change Component Name
    将所有组件修改为遵循Vue官方推荐规范样式

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,14 +1,14 @@
 /*
  * @Author: Miya
  * @Date: 2021-03-15 14:57:29
- * @LastEditTime: 2021-03-17 17:50:32
+ * @LastEditTime: 2021-03-18 16:26:06
  * @LastEditors: Miya
  * @Description: APP page
  * @FilePath: \maid-chanc:\Users\Platinum Prism\Documents\GitHub\Kagura-Image\front\src\App.tsx
  * @Version: 1.0
  */
 import { defineComponent } from 'vue';
-import Logo from './components/logo';
+import Logo from './components/HomeLogo';
 import './style/normalize.less';
 import './style/app.less';
 
