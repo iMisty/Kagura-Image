d5085f2560706441dc67211e74c7f1cd1fa7eb3e
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2021-03-18 18:19:48 +0800

    Create: Manager Component

---
+++ front/src/components/ManagerOptions.tsx
@@ -0,0 +1,9 @@
+/*
+ * @Author: Miya
+ * @Date: 2021-03-18 17:56:51
+ * @LastEditTime: 2021-03-18 17:56:52
+ * @LastEditors: Miya
+ * @Description: 
+ * @FilePath: \maid-chanc:\Users\Platinum Prism\Documents\GitHub\Kagura-Image\front\src\components\ManagerOptions.tsx
+ * @Version: 
+ */
