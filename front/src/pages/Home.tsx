4251a5a8106ce73759a27fd60803b612187757df
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2021-03-18 18:15:53 +0800

    Update Home.tsx

--- front/src/pages/Home.tsx
+++ front/src/pages/Home.tsx
@@ -1,24 +1,24 @@
 /*
  * @Author: Miya
  * @Date: 2021-03-15 12:26:30
- * @LastEditTime: 2021-03-16 17:11:00
+ * @LastEditTime: 2021-03-18 16:22:50
  * @LastEditors: Miya
  * @Description: index
  * @FilePath: \maid-chanc:\Users\Platinum Prism\Documents\GitHub\Kagura-Image\front\src\pages\Home.tsx
  * @Version: 1.0
  */
 import { defineComponent } from 'vue';
-import upload from '../components/upload';
+import UploadFile from '../components/UploadFile';
 
 const home = defineComponent({
   components: {
-    upload,
+    UploadFile,
   },
   setup() {},
   render() {
     return (
       <div class="home">
-        <upload></upload>
+        <UploadFile></UploadFile>
       </div>
     );
   },
