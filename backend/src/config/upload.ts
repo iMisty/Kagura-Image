789ece651abb2461722b4cfa4344e2adf5478101
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2021-03-22 17:59:41 +0800

    Create upload.ts

---
+++ backend/src/config/upload.ts
@@ -0,0 +1,32 @@
+/*
+ * @Author: Miya
+ * @Date: 2021-03-22 14:27:01
+ * @LastEditTime: 2021-03-22 16:16:30
+ * @LastEditors: Miya
+ * @Description: upload
+ * @FilePath: \maid-chanc:\Users\Platinum Prism\Documents\GitHub\Kagura-Image\backend\src\config\upload.ts
+ * @Version: 1.0
+ */
+
+// const multer = require('@koa/multer');
+// const path = require('path');
+// const destPath = path.join(__dirname, '../upload');
+
+// const storage = multer.diskStorage({
+//   destination: (req: any, file: any, cb: any) => {
+//     cb(null, destPath);
+//   },
+//   filename: (req: any, file: any, cb: any) => {
+//     const fileFormat = file.originalname.split('.');
+//     cb(null, Date.now() + '.' + fileFormat[fileFormat.length - 1]);
+//   },
+// });
+
+// const upload = multer({
+//   storage,
+//   limits: {
+//     fileSize: 20000 * 1048576,
+//   },
+// });
+
+// module.exports = upload;
