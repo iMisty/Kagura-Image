5eb7a34148937d57748b4c7e9bf3ceadfaa17b38
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2021-03-22 17:59:49 +0800

    Update index.ts

--- backend/src/router/index.ts
+++ backend/src/router/index.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: Miya
  * @Date: 2021-03-15 17:50:14
- * @LastEditTime: 2021-03-15 17:54:31
+ * @LastEditTime: 2021-03-22 16:25:50
  * @LastEditors: Miya
  * @Description: Router index
  * @FilePath: \maid-chanc:\Users\Platinum Prism\Documents\GitHub\Kagura-Image\backend\src\router\index.ts
@@ -11,12 +11,16 @@
 import * as Router from 'koa-router';
 import * as CombineRouters from 'koa-combine-routers';
 
+const FileView = require('../controller/FileViewController');
+
 const router = new Router();
 
 const index = router.get('/', async (ctx: any) => {
   ctx.body = 'Hello TypeScript';
 });
 
-const routers = CombineRouters(index);
+const rootView = router.post('/dir', FileView.uploadImage);
+
+const routers = CombineRouters(index, rootView);
 
 export default routers;
