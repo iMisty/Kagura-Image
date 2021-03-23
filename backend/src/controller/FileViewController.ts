514ede1957cca7eaf8abc1f563cd55b938e98ac6
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2021-03-23 16:00:23 +0800

    Update: upload image interface

--- backend/src/controller/FileViewController.ts
+++ backend/src/controller/FileViewController.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: Miya
  * @Date: 2021-03-22 10:11:32
- * @LastEditTime: 2021-03-23 11:05:19
+ * @LastEditTime: 2021-03-23 15:48:53
  * @LastEditors: Miya
  * @Description: 文件管理
  * @FilePath: \maid-chanc:\Users\Platinum Prism\Documents\GitHub\Kagura-Image\backend\src\controller\FileViewController.ts
@@ -34,15 +34,29 @@ class FileView {
 
   // 上传单个图片
   public static async uploadImage(ctx: any) {
+    // 用户输入图片信息
     const image = ctx.request.files.image;
-    console.log(image);
+    // 截取地址
+    const resPath = image.path.split('upload_');
+
+    // 输出数据
+    const data = {
+      size: image.size,
+      name: image.name,
+      path: `/upload_${resPath[1]}`,
+    };
+
     try {
       ctx.body = {
-        image,
+        code: 1,
+        msg: '上传成功',
+        data
       };
     } catch (error) {
       ctx.body = {
-        error,
+        code: 0,
+        msg: '上传失败',
+        err: error,
       };
     }
   }
