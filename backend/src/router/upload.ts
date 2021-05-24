d53e34a6f9c75dd9926fac3bbe3223de45bb4bb7
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2021-05-25 02:44:22 +0800

    Update upload.ts

--- backend/src/router/upload.ts
+++ backend/src/router/upload.ts
@@ -1,22 +1,26 @@
 /*
  * @Author: Miya
  * @Date: 2021-03-23 17:05:19
- * @LastEditTime: 2021-03-23 18:04:35
+ * @LastEditTime: 2021-05-23 17:13:50
  * @LastEditors: Miya
  * @Description: 图片相关
- * @FilePath: \maid-chanc:\Users\Platinum Prism\Documents\GitHub\Kagura-Image\backend\src\router\upload.ts
+ * @FilePath: \backend\src\router\upload.ts
  * @Version: 1.0
  */
 
 const router = require('koa-router')();
-const Upload = require('../controller/FileViewController');
+const File = require('../controller/FileController');
+const Upload = require('../controller/UploadImageController');
 const DB = require('../controller/DataBaseController');
-router.prefix('/image')
+router.prefix('/image');
 
+// 检测本地文件夹是否已创建
+router.get('/checkdir', File.getDirExists);
+// 读取根目录文件
+router.get('/readdir', File.getRootDirFiles);
+// 获取图片文件名
+router.get('/', File.getImage);
 // 上传单张图片
 router.post('/', Upload.uploadImage);
-
-// 查找图片
-router.get('/', DB.getImageList);
 
 export default router;
