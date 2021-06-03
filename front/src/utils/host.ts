7181cbc6bef842a5c43f8b2c6243d55475e8e565
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2021-06-04 00:36:14 +0800

    Create: API config

---
+++ front/src/utils/host.ts
@@ -0,0 +1,13 @@
+/*
+ * @Author: Miya
+ * @Date: 2021-06-04 00:32:33
+ * @LastEditTime: 2021-06-04 00:33:37
+ * @LastEditors: Miya
+ * @Description: API address
+ * @FilePath: \front\src\utils\host.ts
+ */
+
+
+export const HOST = 'http://localhost:12450';
+
+export const UPDATE_IMAGE = `${HOST}/image`
