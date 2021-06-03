6fdc65d84f1d631ca671fc861e7b47bb11145e46
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2021-06-04 00:40:48 +0800

    Update request.ts

--- front/src/utils/request.ts
+++ front/src/utils/request.ts
@@ -1,10 +1,10 @@
 /*
  * @Author: Miya
  * @Date: 2021-03-22 16:07:01
- * @LastEditTime: 2021-03-23 16:43:56
+ * @LastEditTime: 2021-06-04 00:33:09
  * @LastEditors: Miya
  * @Description: AJAX Methods
- * @FilePath: \maid-chanc:\Users\Platinum Prism\Documents\GitHub\Kagura-Image\front\src\utils\request.ts
+ * @FilePath: \front\src\utils\request.ts
  * @Version: 1.0
  */
 
