860f7b2836e22a3f384e1a3a4106adf5b79d3b8e
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2021-06-05 07:16:53 +0800

    Update App.tsx

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,10 +1,10 @@
 /*
  * @Author: Miya
  * @Date: 2021-03-15 14:57:29
- * @LastEditTime: 2021-03-18 16:26:06
+ * @LastEditTime: 2021-06-06 04:21:18
  * @LastEditors: Miya
  * @Description: APP page
- * @FilePath: \maid-chanc:\Users\Platinum Prism\Documents\GitHub\Kagura-Image\front\src\App.tsx
+ * @FilePath: \front\src\App.tsx
  * @Version: 1.0
  */
 import { defineComponent } from 'vue';
@@ -31,15 +31,15 @@ const APP = defineComponent({
               <router-link class="navbar--item" tag="article" to="/manager">
                 文件管理
               </router-link>
-              <router-link class="navbar--item" tag="article" to="/admin">
+              {/* <router-link class="navbar--item" tag="article" to="/admin">
                 后台
-              </router-link>
+              </router-link> */}
             </section>
-            <section class="navbar--info">
+            {/* <section class="navbar--info">
               <router-link class="navbar--item" tag="article" to="/login">
                 登录
               </router-link>
-            </section>
+            </section> */}
           </nav>
           <router-view></router-view>
         </section>
