eb6e735f7bc0c7169b541fd2b5f12c292ca7fbf3
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2021-06-07 07:17:18 +0800

    Update request.ts

--- front/src/utils/request.ts
+++ front/src/utils/request.ts
@@ -1,14 +1,15 @@
 /*
  * @Author: Miya
  * @Date: 2021-03-22 16:07:01
- * @LastEditTime: 2021-06-04 00:33:09
+ * @LastEditTime: 2021-06-06 04:54:47
  * @LastEditors: Miya
  * @Description: AJAX Methods
  * @FilePath: \front\src\utils\request.ts
  * @Version: 1.0
  */
 
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
+import { HOST } from './host';
 
 // 上传图片用
 export const UploadRequest = async (url: string, data: any | Blob) => {
@@ -17,11 +18,11 @@ export const UploadRequest = async (url: string, data: any | Blob) => {
   const config = {
     headers: { 'Content-Type': 'multipart/form-data' },
   }; //添加请求头
-  console.log(data)
+  console.log(data);
   params.append('image', data, data.name);
 
   // 发送 AJAX 请求
-  const ajax = await axios  
+  const ajax = await axios
     .post(url, params, config)
     .then((res) => {
       console.log(res);
@@ -32,4 +33,16 @@ export const UploadRequest = async (url: string, data: any | Blob) => {
       return err;
     });
   return ajax;
+};
+
+export const GET = async (url: string, data?: any) => {
+  const result = await axios
+    .get(url)
+    .then((res) => {
+      return res;
+    })
+    .catch((err) => {
+      return err;
+    });
+  return result;
 };
