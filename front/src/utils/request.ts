f883300054585dd8d2b66be5bc92dac727149a8e
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2021-06-12 05:34:36 +0800

    Update: 新增Delete AJAX方法

--- front/src/utils/request.ts
+++ front/src/utils/request.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: Miya
  * @Date: 2021-03-22 16:07:01
- * @LastEditTime: 2021-06-06 04:54:47
+ * @LastEditTime: 2021-06-12 04:47:35
  * @LastEditors: Miya
  * @Description: AJAX Methods
  * @FilePath: \front\src\utils\request.ts
@@ -38,6 +38,18 @@ export const UploadRequest = async (url: string, data: any | Blob) => {
 export const GET = async (url: string, data?: any) => {
   const result = await axios
     .get(url)
+    .then((res) => {
+      return res;
+    })
+    .catch((err) => {
+      return err;
+    });
+  return result;
+};
+
+export const DELETE = async (url: string, data?: any) => {
+  const result = await axios
+    .delete(url, { data })
     .then((res) => {
       return res;
     })
