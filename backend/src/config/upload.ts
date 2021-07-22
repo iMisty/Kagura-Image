87d3b79af29c91b59e29f083216805af5d9bbae6
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2021-07-23 00:32:34 +0800

    Update upload.ts

--- backend/src/config/upload.ts
+++ backend/src/config/upload.ts
@@ -1,33 +1,11 @@
 /*
  * @Author: Miya
  * @Date: 2021-03-22 14:27:01
- * @LastEditTime: 2021-06-10 06:02:24
+ * @LastEditTime: 2021-07-26 00:19:48
  * @LastEditors: Miya
  * @Description: upload
  * @FilePath: \backend\src\config\upload.ts
  * @Version: 1.0
  */
 
-// const multer = require('@koa/multer');
-// const path = require('path');
-// const destPath = path.join(__dirname, '../upload');
-
-// const storage = multer.diskStorage({
-//   destination: (req: any, file: any, cb: any) => {
-//     cb(null, destPath);
-//   },
-//   filename: (req: any, file: any, cb: any) => {
-//     const fileFormat = file.originalname.split('.');
-//     cb(null, Date.now() + '.' + fileFormat[fileFormat.length - 1]);
-//   },
-// });
-
-// const upload = multer({
-//   storage,
-//   limits: {
-//     fileSize: 20000 * 1048576,
-//   },
-// });
-
-// module.exports = upload;
 export const HOST = 'http://localhost:12450';
