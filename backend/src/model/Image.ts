97d386e3262dd8c0ee1766de0dc61a9f450c8993
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2021-07-26 00:41:43 +0800

    Update Image.ts

--- backend/src/model/Image.ts
+++ backend/src/model/Image.ts
@@ -1,11 +1,11 @@
 /*
  * @Author: Miya
  * @Date: 2021-03-23 15:51:43
- * @LastEditTime: 2021-07-26 00:31:09
+ * @LastEditTime: 2021-07-26 00:34:39
  * @LastEditors: Miya
  * @Description: MongoDB图片模型
  * @FilePath: \backend\src\model\Image.ts
- * @Version: 1.0
+ * @Version: 1.1
  */
 
 import * as Mongoose from 'mongoose';
@@ -14,6 +14,7 @@ import * as Mongoose from 'mongoose';
  *  Id: 图片唯一ID
  *  Size: 图片大小
  *  Name: 图片名称
+ *  Thumbnails: 缩略图
  *  Path: 图片链接
  *  Time: 上传时间
  */
@@ -22,7 +23,7 @@ const imageModel = new Mongoose.Schema({
   size: Number,
   name: String,
   path: String,
-  // thumbnails: String,
+  thumbnails: String,
   url: String,
   time: String,
 });
