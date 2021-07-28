891758b510e4d5203761d3235ce0c7c75530ff60
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2021-07-29 01:34:48 +0800

    Update UploadImageController.ts

--- backend/src/controller/UploadImageController.ts
+++ backend/src/controller/UploadImageController.ts
@@ -1,13 +1,13 @@
 /*
  * @Author: Miya
  * @Date: 2021-05-22 14:41:07
- * @LastEditTime: 2021-07-28 00:41:09
+ * @LastEditTime: 2021-08-02 01:33:40
  * @LastEditors: Miya
  * @Description: Update image controller
  * @FilePath: \backend\src\controller\UploadImageController.ts
  */
 
-import { CTXNormal } from '../interface/ctx';
+import { CTXNormal, UploadImageObject } from '../interface/ctx';
 import { formatDate } from '../util/formatDate';
 
 // 上传文件字段
@@ -49,9 +49,11 @@ class UploadController {
         path: `upload_${resPath[1]}`,
         time: formatDate(image.lastModifiedDate),
       };
-      console.table(data);
+      console.log(data);
       console.log('Upload image successed');
       resolve(data);
+    }).catch((err) => {
+      console.table(err);
     });
 
     // return { data };
@@ -67,7 +69,8 @@ class UploadController {
    * @param {*}
    * @return {*}
    */
-  public static async createThumbnails(image: imgUpload) {
+  //TODO : FIX
+  public static async createThumbnails(image: UploadImageObject): Promise<any> {
     // 判断有无thumb目录
     const exists = FC.getDirExists('./src/thumbnail');
     if (!exists) {
@@ -75,6 +78,21 @@ class UploadController {
       await fs.promises.mkdir('./src/thumbnail');
     }
     // 缩略图生成
+    return new Promise((resolve, reject) => {
+      // 图片相对地址
+      const imageSrc = `./src/upload/${image.path}`;
+      // 缩略图相对地址
+      const thumbnailSrc = `.'/src/thumbnail/${image.path}`;
+      // 生成缩略图
+      resizeImg(fs.readFileSync(imageSrc), { width: 128 })
+        .then((buffer: Buffer) => {
+          fs.writeFileSync(thumbnailSrc, buffer);
+        })
+        .then(resolve(image))
+        .catch((err: any) => {
+          reject(err);
+        });
+    });
     const result = await resizeImg(
       fs.readFileSync(
         './src/upload/upload_c49f32fbc85b02942341df45d2a395f6.jpg'
@@ -100,10 +118,12 @@ class UploadController {
 
     try {
       const result = await UploadController.uploadImage(image);
+      const db = await UploadController.createThumbnails(image);
       return (ctx.body = {
         code: 1,
         msg: 'ok',
         data: result,
+        db,
       });
     } catch (err) {
       return (ctx.body = {
