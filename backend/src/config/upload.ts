f1844b3b417744cbf17459717ec5d73cdd9f22de
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2021-08-03 00:25:08 +0800

    Update: Host Address

--- backend/src/config/upload.ts
+++ backend/src/config/upload.ts
@@ -1,11 +1,11 @@
 /*
  * @Author: Miya
  * @Date: 2021-03-22 14:27:01
- * @LastEditTime: 2021-07-26 00:19:48
+ * @LastEditTime: 2021-08-03 00:21:51
  * @LastEditors: Miya
  * @Description: upload
  * @FilePath: \backend\src\config\upload.ts
  * @Version: 1.0
  */
 
-export const HOST = 'http://localhost:12450';
+export const HOST = 'http://localhost:12451';
