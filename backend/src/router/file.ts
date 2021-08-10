cde6f870ac875d88b5b455a6b4ccadfaa9092095
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2021-08-10 18:27:22 +0800

    Update file.ts

--- backend/src/router/file.ts
+++ backend/src/router/file.ts
@@ -1,27 +1,27 @@
 /*
  * @Author: Miya
  * @Date: 2021-06-11 15:08:33
- * @LastEditTime: 2021-08-07 19:12:22
+ * @LastEditTime: 2021-08-13 17:57:21
  * @LastEditors: Miya
  * @Description: 文件相关
  * @FilePath: \backend\src\router\file.ts
  */
 const router = require('koa-router')();
-const FileController = require('../controller/FileController');
-const DBController = require('../controller/DataBaseController');
+// const FileController = require('../controller/FileController');
+// const DBController = require('../controller/DataBaseController');
 
 const FilesController = require('../controller/FilesController');
 
 router.prefix('/file');
 
-// 检测本地文件夹是否已创建
-router.get('/check', FileController.getDirExists);
-// 读取根目录文件
-router.get('/', FileController.getRootDirFiles);
-// 从数据库获取单张文件信息
-router.get('/:id', DBController.getImageList);
-// 删除图片
-router.delete('/:path', FileController.setDeleteImage);
+// // 检测本地文件夹是否已创建
+// router.get('/check', FileController.getDirExists);
+// // 读取根目录文件
+// router.get('/', FileController.getRootDirFiles);
+// // 从数据库获取单张文件信息
+// router.get('/:id', DBController.getImageList);
+// // 删除图片
+// router.delete('/:path', FileController.setDeleteImage);
 
 router.get('/test', FilesController.APIIsDirExist);
 
