a3c7dfb8baa2a7fc1247ad5f353606c73ca9da5c
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2022-03-31 21:19:16 +0800

    Remove Front

--- front/src/components/ManagerOptions.tsx
+++
@@ -1,9 +0,0 @@
-/*
- * @Author: Miya
- * @Date: 2021-03-18 17:56:51
- * @LastEditTime: 2021-03-18 17:56:52
- * @LastEditors: Miya
- * @Description: 
- * @FilePath: \maid-chanc:\Users\Platinum Prism\Documents\GitHub\Kagura-Image\front\src\components\ManagerOptions.tsx
- * @Version: 
- */
