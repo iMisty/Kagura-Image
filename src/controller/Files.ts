f32aeffcc4dfffb0ebcc97837e9a8b4bf18c5050
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2022-06-04 23:45:08 +0800

    Update Files.ts

--- src/controller/Files.ts
+++ src/controller/Files.ts
@@ -1,22 +1,22 @@
 /*
  * @Author: Miya
  * @Date: 2022-03-21 21:53:08
- * @LastEditTime: 2022-03-21 22:03:09
+ * @LastEditTime: 2022-04-06 21:21:42
  * @LastEditors: Miya
  * @Description: Controller about Files
- * @FilePath: \backend\src\controller\Files.ts
+ * @FilePath: \Kagura-Image\src\controller\Files.ts
  */
 
 import * as fs from 'fs/promises';
+import { Context } from 'koa';
 
-class File {
-  private a: any;
-  constructor(a: any) {
-    this.a = a;
-    this.init();
-  }
-
-  init() {
-    console.log(this.a);
+class FileController {
+  public static async Get(ctx: Context) {
+    console.log(ctx.query);
+    return (ctx.body = {
+      ctx: ctx.query,
+    });
   }
 }
+
+export default FileController;
