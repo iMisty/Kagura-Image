ac1659394ce22601fa09d0f841578cc57352c519
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2022-06-05 23:45:17 +0800

    Update upload.ts

--- src/router/upload.ts
+++ src/router/upload.ts
@@ -1,20 +1,18 @@
 /*
  * @Author: Miya
  * @Date: 2021-03-23 17:05:19
- * @LastEditTime: 2022-03-22 12:13:03
- * @LastEditors: Mirage
+ * @LastEditTime: 2022-04-06 21:36:40
+ * @LastEditors: Miya
  * @Description: 图片相关
- * @FilePath: \backend\src\router\upload.ts
+ * @FilePath: \Kagura-Image\src\router\upload.ts
  * @Version: 1.0
  */
 
 const router = require('koa-router')();
-import Upload from '../controller/old/UploadImageController';
+import Upload from '../controller/Upload';
 router.prefix('/image');
 
 // 上传单张图片
-router.post('/', Upload.setUploadImage);
-
-router.get('/thumb', Upload.createThumbnails);
+router.post('/upload', Upload.setUploadImage);
 
 export default router;
